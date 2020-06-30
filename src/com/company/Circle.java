c8bf7db41e55f6d97c974165fef8db2fa009c8c6
Burnoutman <www.m.ru.88@mail.ru>
2020-06-30 22:53:13 +0300

    lesson_4

---
+++ src/com/company/Circle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Circle {
+    public double S;
+
+
+    public double CircleSL() {
+
+        double R=5.0;
+        return S=Math.PI * Math.pow(R,2);
+}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Circle cr =new Circle();
+        System.out.println(cr.CircleS());
 
     }
 }
