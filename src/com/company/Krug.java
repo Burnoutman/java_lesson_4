9685aad9a45850baa95275fe826b29989acd6482
Burnoutman <www.m.ru.88@mail.ru>
2020-06-30 09:21:44 +0300

    lesson_4

---
+++ src/com/company/Krug.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Krug {
+
+    public double S;
+    public double R;
+
+    public Krug(double s, double r) {
+        S = s;
+        R = r;
+    }
+
+    public double CircleS() {
+    return S=Math.PI * Math.pow(R,2);
+}
+}
