c694f13282f3d1df6db5038f8d4765cf2e035262
Burnoutman <www.m.ru.88@mail.ru>
2020-07-01 00:04:59 +0300

    lesson_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Square sq = new Square(2,2);
         System.out.println("Площадь круга S = "+cr.CircleS());
         System.out.println("Периметр круга L = "+cr.CircleL());
-        //Если стороны равны,то квадрат,если нет,то будет считать как прямоугольник
+        //Если стороны равны,то квадрат,если нет,то будет считать как прямоугольник ;)
         if(sq.a==sq.b) {
             System.out.println("Площадь квадрата S = " + sq.SquareS());
             System.out.println("Периметр квадрата P = " + sq.SquareP());
